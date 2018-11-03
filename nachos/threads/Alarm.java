7f86c90446a771505ef225cf411c1e5701dd6dd8
Yiying Jie <37560081+sataivlis@users.noreply.github.com>
2018-11-03 15:19:53 -0700

    Update Alarm.java

--- nachos/threads/Alarm.java
+++ nachos/threads/Alarm.java
@@ -1,5 +1,8 @@
 package nachos.threads;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 import nachos.machine.*;
 
 /**
@@ -14,10 +17,15 @@ public class Alarm {
 	 * <p><b>Note</b>: Nachos will not function correctly with more than one
 	 * alarm.
 	 */
+	
 	public Alarm() {
 		Machine.timer().setInterruptHandler(new Runnable() {
 			public void run() { timerInterrupt(); }
 		});
+		
+		lock = new Lock();
+		waitQueue = new LinkedList<wakeAlarmThread>();
+		
 	}
 
 	/**
@@ -27,7 +35,42 @@ public class Alarm {
 	 * that should be run.
 	 */
 	public void timerInterrupt() {
+
+		// disable interrupts;
+		boolean status = Machine.interrupt().disable();
+
+		//int i = 0;
+		wakeAlarmThread threadNext = null;
+		
+		
+		Iterator<wakeAlarmThread> i =waitQueue.iterator();
+		while(i.hasNext()) {
+			// to see if the thread is ready to be awaken;
+			threadNext = (wakeAlarmThread) i.next(); 
+			
+			// and if ready to wake up
+			if (threadNext.wake <= Machine.timer().getTime()) {
+				waitQueue.remove(i);
+				threadNext.waitThread.ready(); // now waiting thread is ready
+			}
+		}
+		
+//			
+//			// right amount of time? 
+//			if (Machine.timer().getTime() == 500)
+//			//if (waitThread <= Machine.timer().getTime() )
+//			{
+//				// remove from waitQueue to ready;
+//				((KThread) waitQueue.remove(i)).ready();
+//
+//			}
+//		}
+
+		// restore interrupts;
+		Machine.interrupt().restore(status);
+
 		KThread.currentThread().yield();
+
 	}
 
 	/**
@@ -46,8 +89,48 @@ public class Alarm {
 	 */
 	public void waitUntil(long x) {
 		// for now, cheat just to get something working (busy waiting is bad)
+		// given; 
 		long wakeTime = Machine.timer().getTime() + x;
-		while (wakeTime > Machine.timer().getTime())
-			KThread.yield();
+		//	while (wakeTime > Machine.timer().getTime())
+		
+		// disable interrupts; 
+		boolean status = Machine.interrupt().disable();
+		
+		// acquire the lock;
+		lock.acquire();
+		
+		thread = new wakeAlarmThread(wakeTime, KThread.currentThread());
+		
+		// add the thread to the waitQueue;
+		waitQueue.add(thread);
+		
+		// release lock and let thread sleep;
+		lock.release();
+		KThread.sleep();
+		
+		// restore interrupts;
+		Machine.interrupt().restore(status);
+
+		KThread.yield();
 	}
+	
+	private Lock lock;
+	// private LinkedList<Long> sQueue;
+	private LinkedList<wakeAlarmThread> waitQueue;
+	private wakeAlarmThread thread;
+	
+
 }
+
+// implement waiting for thread to wake up time
+class wakeAlarmThread {
+	long wake;
+	KThread waitThread;
+	
+	public wakeAlarmThread (long wTime, KThread wThread) {
+		this.wake = wTime;
+		this.waitThread = wThread;
+	}
+	
+}
+
