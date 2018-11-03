97fc9569d08f91f988362e6a20e59e01abc505b7
Yiying Jie <37560081+sataivlis@users.noreply.github.com>
2018-11-03 16:29:50 -0700

    Update KThread.java

--- nachos/threads/KThread.java
+++ nachos/threads/KThread.java
@@ -193,11 +193,11 @@ public class KThread {
 
 		currentThread.status = statusFinished;
 
-		KThread thread = currentThread.waitList.nextThread();
-		if (thread != null) {
-			thread.ready();
-			thread = currentThread.waitList.nextThread();
-		}
+//		KThread thread = currentThread.waitList.nextThread();
+//		if (thread != null) {
+//			thread.ready();
+//			thread = currentThread.waitList.nextThread();
+//		}
 
 		sleep();
 	}
