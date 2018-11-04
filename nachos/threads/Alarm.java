c14706ffda32aa19300095c2445a5413f5aad872
Yiying Jie <37560081+sataivlis@users.noreply.github.com>
2018-11-03 17:13:18 -0700

    Update Alarm.java

--- nachos/threads/Alarm.java
+++ nachos/threads/Alarm.java
@@ -17,15 +17,15 @@ public class Alarm {
 	 * <p><b>Note</b>: Nachos will not function correctly with more than one
 	 * alarm.
 	 */
-	
+
 	public Alarm() {
 		Machine.timer().setInterruptHandler(new Runnable() {
 			public void run() { timerInterrupt(); }
 		});
-		
+
 		lock = new Lock();
 		waitQueue = new LinkedList<wakeAlarmThread>();
-		
+
 	}
 
 	/**
@@ -40,85 +40,102 @@ public class Alarm {
 		boolean status = Machine.interrupt().disable();
 
 		//int i = 0;
-		wakeAlarmThread threadNext = null;
-		
-		
-		Iterator<wakeAlarmThread> i =waitQueue.iterator();
-		while(i.hasNext()) {
-			// to see if the thread is ready to be awaken;
-			threadNext = (wakeAlarmThread) i.next(); 
-			
+		wakeAlarmThread threadNext;
+
+
+		//Iterator<wakeAlarmThread> i = waitQueue.iterator();
+
+		for (Iterator<wakeAlarmThread> i = waitQueue.iterator(); i.hasNext();) 
+		{
+			threadNext = (wakeAlarmThread) i.next();
+
 			// and if ready to wake up
-			if (threadNext.wake <= Machine.timer().getTime()) {
-				waitQueue.remove(i);
-				threadNext.waitThread.ready(); // now waiting thread is ready
+			if (Machine.timer().getTime() >= threadNext.wake) {
+				//waitQueue.remove(i);
+				//threadNext.waitThread.ready(); // now waiting thread is ready
+				i.remove();
+				threadNext.waitThread.ready();
+
 			}
+
 		}
 		
-//			
-//			// right amount of time? 
-//			if (Machine.timer().getTime() == 500)
-//			//if (waitThread <= Machine.timer().getTime() )
-//			{
-//				// remove from waitQueue to ready;
-//				((KThread) waitQueue.remove(i)).ready();
-//
-//			}
-//		}
-
-		// restore interrupts;
+		// restore interrupts
 		Machine.interrupt().restore(status);
-
-		KThread.currentThread().yield();
 
 	}
 
-	/**
-	 * Put the current thread to sleep for at least <i>x</i> ticks,
-	 * waking it up in the timer interrupt handler. The thread must be
-	 * woken up (placed in the scheduler ready set) during the first timer
-	 * interrupt where
-	 *
-	 * <p><blockquote>
-	 * (current time) >= (WaitUntil called time)+(x)
-	 * </blockquote>
-	 *
-	 * @param	x	the minimum number of clock ticks to wait.
-	 *
-	 * @see	nachos.machine.Timer#getTime()
-	 */
-	public void waitUntil(long x) {
-		// for now, cheat just to get something working (busy waiting is bad)
-		// given; 
-		long wakeTime = Machine.timer().getTime() + x;
-		//	while (wakeTime > Machine.timer().getTime())
-		
-		// disable interrupts; 
-		boolean status = Machine.interrupt().disable();
-		
-		// acquire the lock;
-		lock.acquire();
-		
-		thread = new wakeAlarmThread(wakeTime, KThread.currentThread());
-		
-		// add the thread to the waitQueue;
-		waitQueue.add(thread);
-		
-		// release lock and let thread sleep;
-		lock.release();
-		KThread.sleep();
-		
-		// restore interrupts;
-		Machine.interrupt().restore(status);
+	//		while(i.hasNext()) {
+	//			// to see if the thread is ready to be awaken;
+	//			threadNext = (wakeAlarmThread) i.next(); 
+	//			
+	//			// and if ready to wake up
+	//			if (Machine.timer().getTime() >= threadNext.wake) {
+	//				waitQueue.remove(i);
+	//				threadNext.waitThread.ready(); // now waiting thread is ready
 
-		KThread.yield();
-	}
-	
-	private Lock lock;
-	// private LinkedList<Long> sQueue;
-	private LinkedList<wakeAlarmThread> waitQueue;
-	private wakeAlarmThread thread;
-	
+	//			}
+	//		}
+
+	//			
+	//			// right amount of time? 
+	//			if (Machine.timer().getTime() == 500)
+	//			//if (waitThread <= Machine.timer().getTime() )
+	//			{
+	//				// remove from waitQueue to ready;
+	//				((KThread) waitQueue.remove(i)).ready();
+	//
+	//			}
+	//	
+	// 	KThread.currentThread().yield();
+
+}
+
+/**
+ * Put the current thread to sleep for at least <i>x</i> ticks,
+ * waking it up in the timer interrupt handler. The thread must be
+ * woken up (placed in the scheduler ready set) during the first timer
+ * interrupt where
+ *
+ * <p><blockquote>
+ * (current time) >= (WaitUntil called time)+(x)
+ * </blockquote>
+ *
+ * @param	x	the minimum number of clock ticks to wait.
+ *
+ * @see	nachos.machine.Timer#getTime()
+ */
+public void waitUntil(long x) {
+	// for now, cheat just to get something working (busy waiting is bad)
+	// given; 
+	long wakeTime = Machine.timer().getTime() + x;
+
+	// disable interrupts; 
+	boolean status = Machine.interrupt().disable();
+
+	// acquire the lock;
+	// lock.acquire();
+
+	wakeAlarmThread thread = new wakeAlarmThread(wakeTime, KThread.currentThread());
+
+	// add the thread to the waitQueue;
+	waitQueue.add(thread);
+
+	// release lock and let thread sleep;
+	// lock.release();
+	KThread.sleep();
+
+	// restore interrupts;
+	Machine.interrupt().restore(status);
+
+	// KThread.yield();
+}
+
+private Lock lock;
+// private LinkedList<Long> sQueue;
+private LinkedList<wakeAlarmThread> waitQueue;
+// 	private wakeAlarmThread thread;
+
 
 }
 
@@ -126,11 +143,11 @@ public class Alarm {
 class wakeAlarmThread {
 	long wake;
 	KThread waitThread;
-	
+
 	public wakeAlarmThread (long wTime, KThread wThread) {
 		this.wake = wTime;
 		this.waitThread = wThread;
 	}
-	
+
 }
 
