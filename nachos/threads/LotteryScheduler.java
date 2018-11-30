dfda80e9adca766fb893310503a570ad643877da
Nathan-Huizar <32854191+Nathan-Huizar@users.noreply.github.com>
2018-11-30 14:20:58 -0800

    Update LotteryScheduler.java

--- nachos/threads/LotteryScheduler.java
+++ nachos/threads/LotteryScheduler.java
@@ -2,25 +2,23 @@ package nachos.threads;
 
 import nachos.machine.*;
 
-import java.util.TreeSet;
-import java.util.HashSet;
-import java.util.Iterator;
+import java.util.Random;
 
 /**
  * A scheduler that chooses threads using a lottery.
- *
- * <p>
+ * <p/>
+ * <p/>
  * A lottery scheduler associates a number of tickets with each thread. When a
  * thread needs to be dequeued, a random lottery is held, among all the tickets
  * of all the threads waiting to be dequeued. The thread that holds the winning
  * ticket is chosen.
- *
- * <p>
+ * <p/>
+ * <p/>
  * Note that a lottery scheduler must be able to handle a lot of tickets
  * (sometimes billions), so it is not acceptable to maintain state for every
  * ticket.
- *
- * <p>
+ * <p/>
+ * <p/>
  * A lottery scheduler must partially solve the priority inversion problem; in
  * particular, tickets must be transferred through locks, and through joins.
  * Unlike a priority scheduler, these tickets add (as opposed to just taking
