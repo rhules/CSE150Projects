af28a9435d817d5847a801fda190128c20fe9c4d
rhules <38119764+rhules@users.noreply.github.com>
2018-11-30 19:08:24 -0800

    Revert "Update UserProcess.java"
    This reverts commit 0bebe46fc59f8bee830ee9df2698d2c52de41227.

--- nachos/userprog/UserProcess.java
+++ nachos/userprog/UserProcess.java
@@ -138,12 +138,12 @@ public class UserProcess {
 	 */
 
 	public void exit(int status) {
-		coff.close();
+		children.clear();
 		//close all open files
 		for(int i = 0; i < 16; i++){
 			handleSyscall(8, i, 0, 0, 0);
 		}
-		children.clear();
+		
 	}
 	
 	/**
